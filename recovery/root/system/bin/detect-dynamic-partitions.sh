0f31f8c9e5ed1d1d036487e4857263c0a142e538
Yukizaki Miyamura <yukizakimiyamura@gmail.com>
2025-10-25 01:27:55 +0700

    whyred: change super partition group name
    Signed-off-by: Yukizaki Miyamura <yukizakimiyamura@gmail.com>

--- recovery/root/system/bin/detect-dynamic-partitions.sh
+++ recovery/root/system/bin/detect-dynamic-partitions.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep xiaomi_dynamic_partitions > /dev/null; then
+if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep qti_dynamic_partitions > /dev/null; then
     # Dynamic partitions
     cat /system/etc/recovery.fstab.dynamic >> /system/etc/recovery.fstab
 else
